f24ea820b2504b601e5d5595c83f8d43b01f3dbb
okawa0 <apple_ui@icloud.com>
2025-06-17 22:03:59 +0900

    2025.6.17 オーバーレイ要素の取得、ハンバーガーボタンにクリックイベントを設定 大川

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 const hamburger = document.getElementById('interior-store-index-header-nav');
+const overlay = document.getElementById('interior-store-index-overlay');
+
+let isMenuOpen = false;
+
+hamburger.addEventListener('click', toggleMenu);
+
+overlay.addEventListener('click', closeMenu);
