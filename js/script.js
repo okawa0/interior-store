3e848f3e8ab2b485d99be0be155d36d836779b52
okawa0 <apple_ui@icloud.com>
2025-06-18 22:20:33 +0900

    2025.6.18 開閉トグルの関数定義、開閉処理の定義 大川

--- js/script.js
+++ js/script.js
@@ -6,3 +6,25 @@ let isMenuOpen = false;
 hamburger.addEventListener('click', toggleMenu);
 
 overlay.addEventListener('click', closeMenu);
+
+function toggleMenu() {
+  if (isMenuOpen) {
+      closeMenu();
+  } else {
+      openMenu();
+  }
+}
+
+function openMenu() {
+  hamburger.classList.add('active');
+  overlay.classList.add('active');
+  document.body.style.overflow = 'hidden';
+  isMenuOpen = true;
+}
+
+function closeMenu() {
+  hamburger.classList.remove('active');
+  overlay.classList.remove('active');
+  document.body.style.overflow = '';
+  isMenuOpen = false;
+}
